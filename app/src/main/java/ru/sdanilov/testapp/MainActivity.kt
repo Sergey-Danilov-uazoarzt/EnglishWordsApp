9fc8dff8aeee7c4f172266bf342482d2ccfe7ba2
Sergey-Danilov-uazoarzt <uazoarzt@mail.ru>
2024-09-30 00:26:45 +0300

    Handling of incorrect answer

--- app/src/main/java/ru/sdanilov/testapp/MainActivity.kt
+++ app/src/main/java/ru/sdanilov/testapp/MainActivity.kt
@@ -31,6 +31,108 @@ class MainActivity : AppCompatActivity() {
             markAnswerCorrect()
         }
 
+        binding.layoutAnswer1.setOnClickListener {
+            markAnswerWrong()
+        }
+
+        binding.btnContinue.setOnClickListener {
+            markAnswerNeutral()
+        }
+
+    }
+
+    private fun markAnswerNeutral() {
+
+        with (binding) {
+
+            for (layout in listOf(layoutAnswer1, layoutAnswer3)) {
+                layout.background = ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.shape_rounded_containers
+                )
+            }
+
+            for (textView in listOf(tvVariantValue1, tvVariantValue3)) {
+                textView.setTextColor(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.textVariantsColor
+                    )
+                )
+            }
+
+            for (textView in listOf(tvVariantNumber1, tvVariantNumber3)) {
+                textView.apply {
+                    background = ContextCompat.getDrawable(
+                        this@MainActivity,
+                        R.drawable.shape_rounded_variants
+                    )
+                    setTextColor(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.textVariantsColor
+                        )
+                    )
+                }
+            }
+
+            layoutResult.isVisible = false
+            btnSkip.isVisible = true
+        }
+    }
+
+    private fun markAnswerWrong() {
+
+        binding.layoutAnswer1.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_wrong
+        )
+
+        binding.tvVariantNumber1.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_variants_wrong
+        )
+
+        binding.tvVariantNumber1.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.white
+            )
+        )
+
+        binding.tvVariantValue1.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.btnSkip.isVisible = false
+
+        binding.layoutResult.setBackgroundColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.ivResultIcon.setImageDrawable(
+            ContextCompat.getDrawable(
+                this@MainActivity,
+                R.drawable.ic_wrong
+            )
+        )
+
+        binding.tvResultMessage.text = resources.getString(R.string.title_wrong)
+
+        binding.btnContinue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.layoutResult.isVisible = true
     }
 
     private fun markAnswerCorrect() {
